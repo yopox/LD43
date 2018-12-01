017d5ac0b77aa67d97b91ef6b4e3ad31ddaf5a68
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2018-12-01 19:03:17 +0100

    Moving Title to center.

--- src/title.js
+++ src/title.js
@@ -10,7 +10,7 @@ class Title extends Phaser.Scene {
     create() {
         cursors = this.input.keyboard.createCursorKeys();
 
-        this.add.text(350, 32, 'LD43',
+        this.add.text(320, 128, 'LD43',
             { fontFamily: 'm3x6', fontSize: '48px', color: '#000000', width: 300, align: 'center' } )
     }
 
