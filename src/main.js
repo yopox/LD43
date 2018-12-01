51ba8d30c81057048591c9f6eeccc6d531dabf22
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2018-12-01 18:47:33 +0100

    Adding Title scene

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ var config = {
     width: 680,
     height: 480,
     pixelArt: true,
-    scene: [ Load, Level ]
+    scene: [ Load, Title, Level ]
+    // scene: [ Title, Load, Level ]
 };
 
 var game = new Phaser.Game(config);
