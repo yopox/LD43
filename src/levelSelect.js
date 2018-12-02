a88758b0279fc1d5ce1e00bc2c0e814a6cec5c5a
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2018-12-03 00:19:30 +0100

    Refonte de la selection de niveaux

--- src/levelSelect.js
+++ src/levelSelect.js
@@ -11,14 +11,24 @@ class LevelSelect extends Phaser.Scene {
         this.cursors = this.input.keyboard.createCursorKeys();
 
         this.add.text(320, 32, '--- Level Selection ---',
-            {fontFamily: 'm3x6', fontSize: '48px', color: '#000000'})
+            {fontFamily: 'm3x6', fontSize: '48px', color: '#000000'});
 
-        for (let i = 1; i <= LEVEL_NUMBER; i++)
-            this.add.image(320, 128, "lvl" + i)
+        let y_offset = 32;
+
+        for (let i = 1; i <= LEVEL_NUMBER; i++) {
+            this.add.image(150, y_offset + 120 * i, "lvl" + i).setDisplaySize(96, 96);
+            this.add.rectangle(448, y_offset + 120 * i, 896, 120, 0xdddddd, 0)
+            // .setAlpha(0) //.setDepth(0)
                 .setInteractive()
                 .on('pointerdown', function () {
                     this.scene.start("level", {lvlNumber: i});
                 }, this);
+            this.add.text(250, y_offset + 120 * i - 32, "Level " + i, {
+                fontFamily: 'm3x6',
+                fontSize: '48px',
+                color: '#000000'
+            })
+        }
     }
 
     update() {
