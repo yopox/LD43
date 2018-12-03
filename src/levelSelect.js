53c24a11c91bd0a8abab7ca68913ff955e682e25
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2018-12-03 01:25:57 +0100

    Select with keyboard

--- src/levelSelect.js
+++ src/levelSelect.js
@@ -1,6 +1,8 @@
 class LevelSelect extends Phaser.Scene {
     constructor() {
         super({key: 'levelSelect'});
+        this.selected = -1;
+        this.rectangles = [];
     }
 
     preload() {
@@ -18,12 +20,15 @@ class LevelSelect extends Phaser.Scene {
         let halfSeparator = 4;
         let step = imageSize + 2 * halfSeparator;
 
+        this.selected = -1;
+        this.rectangles = [];
         for (let i = 0; i < LEVEL_NUMBER; i++) {
-            this.add.rectangle(GAME_WIDTH / 2, yOffset + step * i, GAME_WIDTH, step, 0, 0)
-                .setInteractive()
-                .on('pointerdown', function () {
-                    this.scene.start("level", {lvlNumber: i});
-                }, this);
+            this.rectangles.push(
+                this.add.rectangle(GAME_WIDTH / 2, yOffset + step * i, GAME_WIDTH, step, 0xffffff)
+                    .setAlpha(0).setInteractive()
+                    .on('pointerdown', function () {
+                        this.scene.start("level", {lvlNumber: i});
+                    }, this));
             this.add.image(150, yOffset + step * i, "lvl" + i).setDisplaySize(imageSize, imageSize);
             this.add.text(250, yOffset + step * i - 32, "Level " + i, {
                 fontFamily: 'm3x6',
@@ -33,9 +38,31 @@ class LevelSelect extends Phaser.Scene {
         }
     }
 
+    select(rect) {
+        this.rectangles[rect].setAlpha(1);
+        console.log("Select " + rect);
+    }
+
+    unselect(rect) {
+        this.rectangles[rect].setAlpha(0);
+    }
+
     update() {
+        let up = Phaser.Input.Keyboard.JustDown(this.cursors.up);
+        let down = Phaser.Input.Keyboard.JustDown(this.cursors.down);
         if (this.cursors.space.isDown) {
-            this.scene.start("level", {lvlNumber: 1});
+            this.scene.start("level", {lvlNumber: this.selected !== -1 ? this.selected: 1});
+        } else if ((up || down) && this.selected === -1) {
+            this.select(1);
+            this.selected = 1;
+        } else if (up && this.selected > 0) {
+            this.unselect(this.selected);
+            this.selected--;
+            this.select(this.selected);
+        } else if (down && this.selected < LEVEL_NUMBER - 1) {
+            this.unselect(this.selected);
+            this.selected++;
+            this.select(this.selected);
         }
     }
 }
